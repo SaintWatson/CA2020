bc4c595f84b30e171af36439c8b0fca8007de3de
Elliot Watson <a0916228544@gmail.com>
2020-10-26 04:28:50 +0000

    record some config

---
+++ git-config.sh
@@ -0,0 +1,7 @@
+git config --global alias.cf config
+git config --global alias.cm commit
+git config --global alias.co checkout
+git config --global alias.st status
+git config --global alias.br branch
+git config --global user.email "a0916228544@gmail.com"
+git config --global user.name "Elliot Watson"
